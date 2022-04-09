c680bd19970d6771c9ac64f0067d6aeaae0dee66
Jeferson Santos <jefersonsantos.jsm@gmail.com>
2022-04-08 23:47:48 -0400

    Add standard offline support for the following languages: (en-US; es-ES; es-EU; fr-FR; pt-BR; pt-PT)

---
+++ src/lang/index.js
@@ -0,0 +1,7 @@
+// Import only languages that your project needs to support offline
+import './summernote-en-US.js';
+import './summernote-es-ES.js';
+import './summernote-es-EU.js';
+import './summernote-fr-FR.js';
+import './summernote-pt-BR.js';
+import './summernote-pt-PT.js';
